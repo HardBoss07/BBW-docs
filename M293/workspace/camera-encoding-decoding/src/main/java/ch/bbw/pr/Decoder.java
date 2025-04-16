f4b79582d44b45b3e4cb6e1de2774c2412625287
HardBoss07 <matteo@bosshard.net>
2025-04-16 10:52:15 +0200

    added validation to Decoder.java, so if the barcode that has been read by BarcodeReader.java is invalid it will prompt that

--- M293/workspace/camera-encoding-decoding/src/main/java/ch/bbw/pr/Decoder.java
+++ M293/workspace/camera-encoding-decoding/src/main/java/ch/bbw/pr/Decoder.java
@@ -8,7 +8,37 @@ public class Decoder {
 
    static public String decode(String barcode) {
       //implement decoding here.
+      if (barcode == null || !validate(barcode)) {
+         return "INVALID BARCODE";
+      }
+      String[] splitted = barcode.split("-");
+      String result = "";
+      for (String s : splitted) {
+         result += s + "\n";
+      }
 
-      return barcode + "\n2nd" + "\n3rd";
+      return result;
+   }
+
+   static public boolean validate(String barcode) {
+      String[] splitted = barcode.split("-");
+
+      if (splitted.length != 6) {
+         return false;
+      }
+
+      for (int i = 0; i < 2; i++) {
+         if (!splitted[i].matches("[a-zA-Z]+")) {
+            return false;
+         }
+      }
+
+      for (int i = 2; i < 6; i++) {
+         if (!splitted[i].matches("\\d+")) {
+            return false;
+         }
+      }
+
+      return true;
    }
 }
