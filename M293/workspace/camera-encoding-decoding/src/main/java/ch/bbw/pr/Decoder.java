01f096e56821e8441f6a3bd08fd7cd48068c4e4c
HardBoss07 <matteo@bosshard.net>
2025-04-16 10:00:49 +0200

    init commit for camera-encoding-decoding project

---
+++ M293/workspace/camera-encoding-decoding/src/main/java/ch/bbw/pr/Decoder.java
@@ -0,0 +1,14 @@
+package ch.bbw.pr;
+
+/**
+ * @author Matteo Bosshard
+ * @version 16.04.2025
+ */
+public class Decoder {
+
+   static public String decode(String barcode) {
+      //implement decoding here.
+
+      return barcode + "\n2nd" + "\n3rd";
+   }
+}
