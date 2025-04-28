71392a3ee36fcc44ab41e3e05d7764d89d1e323a
HardBoss07 <matteo@bosshard.net>
2025-04-28 11:26:55 +0200

    made random chuck norris joke exercise

--- M293/workspace/chuck-norris/src/App.jsx
+++ M293/workspace/chuck-norris/src/App.jsx
@@ -2,43 +2,25 @@ import {useState, useEffect} from 'react'
 import './App.css'
 
 function App() {
-    const [data, setData] = useState([]);
-    const [data2, setData2] = useState([]);
+    const [chuckNorrisRandom, setChuckNorrisRandom] = useState([]);
 
-    const getNewData = async() => {
-        await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+    const getChuckNorrisRandom = async() => {
+        await fetch('https://api.chucknorris.io/jokes/random')
             .then(response => response.json())
             .then(data => {
                 console.log(data)
-                setData2(data)
+                setChuckNorrisRandom(data)
             })
             .catch(error => console.error(error));
     }
 
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
-            .then(response => response.json())
-            .then(json => {
-                console.log(json)
-                setData(json)
-            })
-            .catch(error => console.error(error));
-    }, []);
-
     return (
         <>
+
             <div>
-                <h1>Data1</h1>
-                {data.map(item => (
-                    <p key={item.id}>{item.title}</p>
-                ))}
-            </div>
-            <button onClick={getNewData}>get data2</button>
-            <div>
-                <h1>Data2</h1>
-                {data2.map(item => (
-                    <p key={item.id}>{item.title}</p>
-                ))}
+                <h1>Random Chuck Norris Joke</h1>
+                <button onClick={getChuckNorrisRandom}>Random Chuck Norris Joke</button><br/>
+                {chuckNorrisRandom.value}
             </div>
         </>
 
