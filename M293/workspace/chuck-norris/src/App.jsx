2d37b93836f0170872f29860bfc2d7a9ea522c74
HardBoss07 <matteo@bosshard.net>
2025-04-28 11:06:54 +0200

    made first 2 api exercises

--- M293/workspace/chuck-norris/src/App.jsx
+++ M293/workspace/chuck-norris/src/App.jsx
@@ -3,9 +3,20 @@ import './App.css'
 
 function App() {
     const [data, setData] = useState([]);
+    const [data2, setData2] = useState([]);
+
+    const getNewData = async() => {
+        await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                setData2(data)
+            })
+            .catch(error => console.error(error));
+    }
 
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts ?_ limit=10')
+        fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
             .then(response => response.json())
             .then(json => {
                 console.log(json)
@@ -15,11 +26,21 @@ function App() {
     }, []);
 
     return (
-        <div>
-            {data.map(item => (
-                <p key={item.id}>{item.title}</p>
-            ))}
-        </div>
+        <>
+            <div>
+                <h1>Data1</h1>
+                {data.map(item => (
+                    <p key={item.id}>{item.title}</p>
+                ))}
+            </div>
+            <button onClick={getNewData}>get data2</button>
+            <div>
+                <h1>Data2</h1>
+                {data2.map(item => (
+                    <p key={item.id}>{item.title}</p>
+                ))}
+            </div>
+        </>
 
     );
 }
